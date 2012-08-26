73735c8f2d2677db2061514321a12e04adb98d5c
barryosull <barry@tercet.io>
2012-08-26 23:13:16 +0100

    Fixed CSS bugs. Is now a working version

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php 
-$search_term = ($_POST['search_term'])? $_POST['search_term'] : 'socialmedia';
+$search_term = (isset($_POST['search-term']))? $_POST['search-term'] : 'socialmedia';
 ?>
 <!doctype html>
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
@@ -14,58 +14,78 @@ $search_term = ($_POST['search_term'])? $_POST['search_term'] : 'socialmedia';
 <title>Looking at tweets on the topic off <?=$search_term?></title>
 <style type="text/css">
 
-body{
+body {
 	margin: 0px;
 	font-family: sans-serif;
-	background-color: #fff;
-}
-
-#frame{
-	width: 600px;
-	margin: 0px auto;
 	background-color: #eee;
 }
 
-.title{
-	font-size: 12px;
+#frame {
+	width: 600px;
+  height: 100%;
+	margin: 10px auto;
+	background-color: #fff;
+}
+
+.title {
+	font-size: 20px;
+  padding: 10px;
+}
+
+form {
+  padding: 10px;
+}
+
+input {
+  font-size: 14px;
+}
+
+.twitter {
+  margin: 10px;
 }
 
 </style>
 </head>
 <body>
-<div class="title">Hi User</div>
-<div class="title">Hi User</div>
-<div class="twitter">
-        <script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
-        <script>
-        new TWTR.Widget({
-          version: 2,
-          type: 'search',
-          search: '<?=$search_term?>',
-          interval: 30000,
-          title: '<?=ucfirst($search_term);?> conversations',
-          subject: 'Who\'s talking about <?=ucfirst($search_term);?>',
-          width: 'auto',
-          height: 220,
-          theme: {
-            shell: {
-              background: '#0f75bc',
-              color: '#ffffff'
-            },
-            tweets: {
-              background: '#ffffff',
-              color: '#000000',
-              links: '#4750ff'
-            }
-          },
-          features: {
-            scrollbar: true,
-            loop: false,
-            live: false,
-            behavior: 'all'
-          }
-        }).render().start();
-        </script>
-      </div>
+<div id="frame">
+  <div class="title">Hi User</div>
+  <form action="/" method="post">
+    Search for a term on twitter and see the results!<br><br>
+    <input type="text" name="search-term" value="<?=$search_term?>">
+    <input type="submit" value="Search">
+  </form>
+  <div class="twitter">
+    <script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
+    <script>
+    new TWTR.Widget({
+      version: 2,
+      type: 'search',
+      search: '<?=$search_term?>',
+      interval: 30000,
+      title: '<?=ucfirst($search_term);?> conversations',
+      subject: 'Who\'s talking about <?=ucfirst($search_term);?>',
+      width: 'auto',
+      height: 220,
+      theme: {
+        shell: {
+          background: '#0f75bc',
+          color: '#ffffff'
+        },
+        tweets: {
+          background: '#ffffff',
+          color: '#000000',
+          links: '#4750ff'
+        }
+      },
+      features: {
+        scrollbar: true,
+        loop: false,
+        live: false,
+        behavior: 'all'
+      }
+    }).render().start();
+    </script>
+  </div>
+</div>
 </body>
 </html>
